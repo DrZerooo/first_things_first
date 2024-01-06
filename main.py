399d7e64f9d130cf0c1936229b8be670d6363012
DrZerooo <isaiasdamaceno@outlook.com>
2024-01-06 17:25:37 -0300

    desafio panaca

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print ("Hello world")
+pessoa = {
+    'Nome' : 'Bruno',
+    'Sobrenome' : 'Picanha',
+    'Idade' : '82'
+}
 
+# print(pessoa['Sobrenome'])
+
+# for chave in pessoa:
+#     print (chave, '=', pessoa[chave]) 
+
+for chave, valor in pessoa.items():
+    print(f'{chave} = {valor}')
